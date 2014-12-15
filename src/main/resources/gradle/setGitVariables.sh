92193473c80328f21674b17069742b620c301cf8
Marcin Zajaczkowski <marcin.zajaczkowski@4finance.com>
2014-12-15 17:52:10 +0100

    Fix repository name
    Command: #DO_RELEASE

--- src/main/resources/gradle/setGitVariables.sh
+++ src/main/resources/gradle/setGitVariables.sh
@@ -5,6 +5,6 @@ echo Configuring Git for pushing to GH...
 git config user.email "4financebot@gmail.com"
 git config user.name "4Finance Bot"
 git config push.default simple
-git remote set-url origin "https://${GH_TOKEN}@github.com/4finance/releasing-sandbox.git"
+git remote set-url origin "https://${GH_TOKEN}@github.com/4finance/micro-common-release.git"
 
 echo Git configured
