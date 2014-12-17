9763f89ff2bcd0b9f746b7882c5caf9b76e12b7a
Marcin Zajaczkowski <szpak@users.sourceforge.net>
2014-12-17 22:36:55 +0100

    Switch origin URL to https
    [#DO_RELEASE]

--- src/main/resources/gradle/setGitVariables.sh
+++ src/main/resources/gradle/setGitVariables.sh
@@ -5,5 +5,6 @@ echo Configuring Git for pushing to GH...
 git config user.email "4financebot@gmail.com"
 git config user.name "4Finance Bot"
 git config push.default simple
+git remote set-url origin "https://github.com/${TRAVIS_REPO_SLUG}.git"
 
 echo Git configured
